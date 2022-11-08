5ee3996d91e0a165e597c7c74aeb20ffcd04b57c
Siddharth Maithani <siddharthmaithani94@gmail.com>
2022-11-08 16:42:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,28 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <>
-    <h1>This the my news app here</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptas quaerat, consectetur numquam temporibus fugit ratione iure nostrum ipsam veniam soluta facere ullam dolorem. Consequatur dolorum corporis quod accusantium nesciunt.</h1>
     </>
   );
 }
-
-
 export default App;
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+//   </header>
+// </div>
