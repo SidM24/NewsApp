591c05c486f2a4741be9d9b7979ee158430c81fe
Siddharth Maithani <siddharthmaithani94@gmail.com>
2024-06-18 16:21:29 +0530

    FinalCommit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,52 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../images/Logo.png'
-import '../componentCSS/Navbar.css'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../images/Logo.png';
+import '../componentCSS/Navbar.css';
 
-export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ padding: '0px', display: 'flex', justifyContent: 'center' }}>
-                    <div>
-                        <div style={{ justifyContent: 'center', display: 'flex' }}>
-                            <Link className="navbar-brand" to="/">
-                                <img src={logo} alt='CurrentWave' style={{ width: '15vh' }} />
-                            </Link>
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                        </div>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent" >
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item"><Link className="nav-link" aria-current="page" to="/general">Headlines</Link></li>
-                                <li className="nav-item"><Link className="nav-link" aria-current="page" to="/business">Business</Link></li>
-                                <li className="nav-item"><Link className="nav-link" aria-current="page" to="/entertainment">Entertainment</Link></li>
-                                <li className="nav-item"><Link className="nav-link" aria-current="page" to="/health">Health</Link></li>
-                                <li className="nav-item"><Link className="nav-link" aria-current="page" to="/science">Science</Link></li>
-                                <li className="nav-item"><Link className="nav-link" aria-current="page" to="/sports">Sports</Link></li>
-                                <li className="nav-item"><Link className="nav-link" aria-current="page" to="/technology">Technology</Link></li>
-                            </ul>
-                        </div>
+const Navbar = () => {
+    const location = useLocation();
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ padding: '0px', display: 'flex', justifyContent: 'center' }}>
+                <div>
+                    <div style={{ justifyContent: 'center', display: 'flex' }}>
+                        <Link className="navbar-brand" to="/">
+                            <img src={logo} alt='CurrentWave' style={{ width: '15vh' }} />
+                        </Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
                     </div>
-                </nav>
-            </div>
-        )
-    }
-}
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent" >
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className={`nav-link ${location.pathname === "/general" || location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/general">Headlines</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/business">Business</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/entertainment">Entertainment</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/health">Health</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/science">Science</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/sports">Sports</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/technology">Technology</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
 
-
+export default Navbar;
